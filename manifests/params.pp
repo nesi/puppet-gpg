e4280ec6f9056841bb34aa5b0740384629921c11
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-04-22 15:49:04 +1200

    missing semicolon

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,7 +4,7 @@ case $::osfamily {
   'Debian', 'RedHat':{
     $package  = 'gnupg'
   }
-  default {
+  default: {
     fail("The operating system family ${::osfamily} is not supported by the puppet-gpg module on ${::fqdn}")
   }
 }
